b4e8c166323bf5d87374a44d9b895492a736d83a
idelic <ivan.delic@oracle.com>
2020-02-12 23:36:34 +0100

    address bug fixed

--- realyticsai-demo/src/service/LguService.js
+++ realyticsai-demo/src/service/LguService.js
@@ -5,15 +5,15 @@ export class LguService {
     }
 
     listLgu() {
-        return fetch("http://localhost:8080/lgu-manager/lgus").then(promise => promise.json());
+        return fetch(process.env.REACT_APP_API_URL + "/lgu-manager/lgus").then(promise => promise.json());
     }
 
     listLguCounties() {
-        return fetch("http://localhost:8080/lgu-manager/lgu-counties").then(promise => promise.json())
+        return fetch(process.env.REACT_APP_API_URL + "/lgu-manager/lgu-counties").then(promise => promise.json())
     }
 
     getLgu(id) {
         //return fetch(this.baseUrl + "/lgu-360/lgu/" + id).then(promise => promise.json());
-        return fetch("http://localhost:8080/lgu-manager/lgu/" + id).then(promise => promise.json());
+        return fetch(process.env.REACT_APP_API_URL + "/lgu-manager/lgu/" + id).then(promise => promise.json());
     }
 }
